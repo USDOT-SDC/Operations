8ec37ad22c30a89a266550123df10bbde1889477
Jeff Ussing <Jeff.Ussing.ctr@dot.gov>
2022-12-07 22:44:01 -0500

    add subnets

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,1 +1,53 @@
 data "aws_caller_identity" "current" {}
+
+data "aws_ssm_parameter" "environment" {
+  name = "environment"
+}
+
+data "aws_security_group" "default" {
+  vpc_id = data.aws_vpc.default.id
+  name   = "default"
+}
+
+data "aws_subnet" "support" {
+  filter {
+    name   = "tag:Name"
+    values = ["Support Workstations"]
+  }
+}
+
+data "aws_subnet" "researcher" {
+  filter {
+    name   = "tag:Name"
+    values = ["Researcher Workstations"]
+  }
+}
+
+data "aws_subnet" "infrastructure_3" {
+  filter {
+    name   = "tag:Name"
+    values = ["Infrastructure 3"]
+  }
+}
+
+data "aws_subnet" "infrastructure_4" {
+  filter {
+    name   = "tag:Name"
+    values = ["Infrastructure 4"]
+  }
+}
+
+data "aws_subnet" "infrastructure_5" {
+  filter {
+    name   = "tag:Name"
+    values = ["Infrastructure 5"]
+  }
+}
+
+data "aws_subnet" "infrastructure_6" {
+  filter {
+    name   = "tag:Name"
+    values = ["Infrastructure 6"]
+  }
+}
+
