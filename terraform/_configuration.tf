39092a57e1a9a3abcb6a168d4b1edaa4f5e22516
Jeff Ussing <Jeff.Ussing.ctr@dot.gov>
2022-12-07 22:45:31 -0500

    update default tags

--- terraform/_configuration.tf
+++ terraform/_configuration.tf
@@ -2,32 +2,35 @@ terraform {
   required_version = "~> 1.0"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 4.0"
     }
     null = {
-      source = "hashicorp/null"
+      source  = "hashicorp/null"
       version = "~> 3.0"
     }
     archive = {
-      source = "hashicorp/archive"
+      source  = "hashicorp/archive"
       version = "~> 2.0"
     }
     local = {
-      source = "hashicorp/local"
+      source  = "hashicorp/local"
       version = "~> 2.0"
     }
   }
   backend "s3" {
     // Variables can not be used here
     region = "us-east-1"
-    key = "infrastructure/terraform/terraform.tfstate"
+    key    = "operations/terraform/terraform.tfstate"
   }
 }
 
 provider "aws" {
   region = "us-east-1"
   default_tags {
-    tags = local.default_tags
+    tags = {
+      repository_url = "https://github.com/USDOT-SDC/"
+      repository     = "operations"
+    }
   }
 }
