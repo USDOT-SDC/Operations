f1792a599d03cd8cade2e388a37c907305f6ceee
Jeff Ussing <Jeff.Ussing.ctr@dot.gov>
2022-12-07 23:37:56 -0500

    add research team module

--- terraform/_modules.tf
+++ terraform/_modules.tf
@@ -2,3 +2,10 @@ module "common-pipelines_waze" {
   source = "./common-pipelines/waze"
   common = local.common
 }
+
+module "research-team" {
+  count  = length(yamldecode(file("../../operations-secrets/research-team/research-teams.yaml")))
+  source = "./research-team"
+  common = local.common
+  team   = yamldecode(file("../../operations-secrets/research-team/research-teams.yaml"))[count.index]
+}
